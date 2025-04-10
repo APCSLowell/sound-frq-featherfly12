e8c6179bda71f26a6dbe48633f5bdc954f361f39
featherfly12 <146142331+featherfly12@users.noreply.github.com>
2025-04-10 16:33:58 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -1,10 +1,8 @@
 import java.util.*;
-public class Sound
-{
+
+public class Sound {
   /** the array of values in this sound; guaranteed not to be null */
   int[] samples;
-
-
 
   /** Changes those values in this sound that have an amplitude greater than limit.
    *  Values greater than limit are changed to limit.
@@ -13,39 +11,34 @@ public class Sound
    *         Precondition: limit >= 0
    *  @return the number of values in this sound that this method changed
    */
-  public int limitAmplitude(int limit)
-  {  
+  public int limitAmplitude(int limit) {
     int count = 0;
-    for(int i = 0;i<samples.length;i++){
-      if(samples[i]>limit){
-        samples[i]=limit;
+    for (int i = 0; i < samples.length; i++) {
+      if (samples[i] > limit) {
+        samples[i] = limit;
         count++;
-      }
-      else if(samples[i]<-limit){
-        samples[i]=-limit;
+      } else if (samples[i] < -limit) {
+        samples[i] = -limit;
+        count++; // <- add count increment here too
       }
     }
     return count;
-    /* to be implemented in part (a) */
   }
-
-
 
   /** Removes all silence from the beginning of this sound.
    *  Silence is represented by a value of 0.
    *  Precondition: samples contains at least one nonzero value
    *  Postcondition: the length of samples reflects the removal of starting silence
    */
-  public void trimSilenceFromBeginning()
-  {
+  public void trimSilenceFromBeginning() {
     int n = 0;
-while(ssamples[n]==0){
-  n++;}
-    int[] nSamples = new int[samples.length-n];
-    for(int i = 0;i<nSamples.length;i++){
-      nSamples[i]=samples[i+n];
+    while (samples[n] == 0) {
+      n++;
+    }
+    int[] nSamples = new int[samples.length - n];
+    for (int i = 0; i < nSamples.length; i++) {
+      nSamples[i] = samples[i + n];
     }
     samples = nSamples;
-    /* to be implemented in part (b) */
   }
 }
